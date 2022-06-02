bcdeaf395d3fff967a08d30cc380f9e06310f0e3
Victor Hugo Amaral <amaral.vht@gmail.com>
2022-06-02 14:51:28 -0300

    Update prot_trabalho_1.c

--- prot_trabalho_1.c
+++ prot_trabalho_1.c
@@ -8,6 +8,7 @@ int main() {
     int max_geracoes;
     int tam_populacao;
     int melhor_individuo;
+    int geracao = 0;
     double taxa_mutacao;
     double a, b, c, d, e, f;
 
@@ -50,14 +51,10 @@ int main() {
     int populacao[tam_populacao];
 
 
-
     //Gerar aleatoriamente a populacao inicial
     for(int i = 0; i < tam_populacao; i++) {
         populacao[i] = (rand() % (255 * 2)) - 255;
-        printf("%d ", populacao[i]);
     }
-
-    printf("\n\n\n");
     
 
     //Laco das geracoes:
@@ -65,16 +62,21 @@ int main() {
         long long int resultado;
         long long int diferencas[tam_populacao];
         long long int melhor_diferenca;
+        long long int pior_diferenca;
+
+        geracao++;
 
 
-        //Avaliar populacao para o seu problema e Armazenar o melhor individuo ate o momento
+        //Avaliar populacao para o seu problema e Armazenar o melhor individuo ate o momento:
+
+        //Testa o resultado com o primeiro individuo da populacao:
         resultado = a*pow(populacao[0], 5) + b*pow(populacao[0], 4) + c*pow(populacao[0], 3) + d*pow(populacao[0], 2) + e*populacao[0] + f;
         diferencas[0] = llabs(resultado);
         if(i == 0 || diferencas[0] < melhor_diferenca) {
             melhor_diferenca = diferencas[0];
             melhor_individuo = populacao[0];
         }
-
+        pior_diferenca = diferencas[0];
 
         for(int j = 1; j < tam_populacao; j++) {
             resultado = a*pow(populacao[j], 5) + b*pow(populacao[j], 4) + c*pow(populacao[j], 3) + d*pow(populacao[j], 2) + e*populacao[j] + f;
@@ -84,90 +86,89 @@ int main() {
                 melhor_individuo = populacao[j];
                 melhor_diferenca = diferencas[j];
             }
+            else if(diferencas[j] > pior_diferenca) {
+                pior_diferenca = diferencas[j];
+            }
         }
 
-        
 
-
-        //Verificar se a solucao esta aceitavel(se cim, encerrar laco)
-        if(melhor_diferenca <= 10) {
-            printf("Parando laco com a diferenca %d", melhor_diferenca);
+        //Verificar se a solucao esta aceitavel(se sim, encerrar laco)
+        if(melhor_diferenca <= 3) {
+            printf("Solucao aceitavel: \n");
             break;
         }
 
 
         //Selecionar os melhores individuos:
-        int troca;
-
-        do {
-            troca = 0;
-
-            for(int x = 1; x < tam_populacao; x++) {
-                int k = x - 1;
-
-                if(diferencas[k] > diferencas[x]) {
-                    int aux = populacao[x];
-                    populacao[x] = populacao[k];
-                    populacao[k] = aux;
-
-                    aux = diferencas[x];
-                    diferencas[x] = diferencas[k];
-                    diferencas[k] = aux;
-
-                    troca = 1;
-                }
-            }
-        } while(troca != 0); 
-
         int melhores[tam_populacao / 2];
+        int cont = 0;
+        int m = 0;
 
-        for(int j = 0; j < tam_populacao / 2; j++) { //-------------Lembrar de tirar!!!
-            melhores[j] = populacao[j];
-            printf("%d ", melhores[j]);
+        while(cont < (tam_populacao/2)) {
+            for(int j = 0; j < tam_populacao; j++) {
+                int chance = (rand() % (pior_diferenca - melhor_diferenca)) + melhor_diferenca;
+
+                if(chance > populacao[m]) {
+                    melhores[cont] = populacao[m];
+                    cont++;
+                }
+
+                m++;
+            }
+
+            m = 0;
         }
+
 
         //Aplicar cruzamento:
         int x = 0;
-        int mascara_bits_direita = 0b1111111111111111;
-        int mascara_bits_esquerda = 0b11111111111111110000000000000000;
+        int mascara_bits_direita = 15;
+        int mascara_bits_esquerda = mascara_bits_direita << 4;
 
-        printf("\nmelhor individuo antes da reprod: %d", melhor_individuo);
-        printf("\nmelhor dif antes da reprod: %d", melhor_diferenca);
-
-
-        printf("\nPopulacao nova: \n");
-        for(int j = 0; j < tam_populacao / 2; j++) {
+        for(int j = 0; j < tam_populacao/2; j++) {
             int individuo1, individuo2;
 
             do {
                 individuo1 = melhores[rand() % (tam_populacao/2)];
                 individuo2 = melhores[rand() % (tam_populacao/2)];  
 
-                int temp_direita = ( individuo1 & mascara_bits_direita ) & ( individuo2 & mascara_bits_direita );
+                int temp_direita1 = individuo1 & mascara_bits_direita;
+                int temp_direita2 = individuo2 & mascara_bits_direita;
 
-                int temp_esquerda1 = (individuo1 & mascara_bits_esquerda);
-                int temp_esquerda2 = (individuo2 & mascara_bits_esquerda);
+                int temp_esquerda1 = individuo2 & mascara_bits_esquerda;
+                int temp_esquerda2 = individuo1 & mascara_bits_esquerda;
 
-                populacao[x] = temp_esquerda1 | temp_direita;
-                populacao[x+1] = temp_esquerda2 | temp_direita; 
+                populacao[x] = temp_direita1 | temp_esquerda1;
+                populacao[x+1] = temp_direita2 | temp_esquerda2; 
 
             } while((abs(populacao[x]) > 255) || (abs(populacao[x+1]) > 255));
-
-            printf("\n%d + %d = %d %d\n", individuo1, individuo2, populacao[x], populacao[x+1]); 
 
             x = x + 2;
         }
 
+        populacao[0] = melhor_individuo;
+
         
-
         //Aplicar mutacao:
+        int mascara_mutacao = 0b1;
 
-        for(int j = 0; j < tam_populacao; j++)
+        for(int j = 0; j < tam_populacao; j++) {
+            double chance = (rand() % 100) / (double)100;
+            int bit;
 
+            if(chance < taxa_mutacao) {
+                int mutacao = rand() % 7;
+
+                bit = mascara_mutacao << mutacao;
+
+                populacao[j] = populacao[j] ^ bit;
+            }
+        }
     } 
 
+
     //Imprimir a melhor solucao
-    printf("resultado: %d", melhor_individuo);
+    printf("Resultado: %d, da geracao %d", melhor_individuo, geracao);
 
     return 0; 
 }
